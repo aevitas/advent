ac674f68c74a1db2259037df3bfc564713607fab
aevitas <m4rtijn@gmail.com>
2019-12-04 19:44:24 +0100

    Day one part 2

--- Day1.cs
+++ Day1.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace AdventOfCode
@@ -13,10 +12,26 @@ namespace AdventOfCode
             using var sr = new StreamReader("Day1.txt");
             var input = await sr.ReadToEndAsync();
 
-            var masses = input.Split(Environment.NewLine).Select(int.Parse);
-            var totalMass = masses.Sum(m => (int) Math.Floor((double) m / 3) - 2);
+            var masses = input.Split(Environment.NewLine).Select(int.Parse).ToList();
+            var one = masses.Sum(i => i / 3 - 2); // Can skip Math.Floor because of int
 
-            Console.WriteLine(totalMass);
+            Console.WriteLine(one);
+
+            var two = masses.Sum(i =>
+            {
+                var totalFuel = 0;
+                int y = i / 3 - 2; // Initial fuel requirement for module;
+
+                while (y > 0) // Keep adding fuel to carry the fuel until we hit negative fuel req
+                {
+                    totalFuel += y;
+                    y = y / 3 - 2; 
+                }
+                
+                return totalFuel;
+            });
+
+            Console.WriteLine(two);
         }
     }
 }
