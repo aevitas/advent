44f7eb8d466f85ebf4ce386b604e5ba375716e50
aevitas <m4rtijn@gmail.com>
2019-12-04 19:01:47 +0100

    Solve 2019 day 1

---
+++ Day1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public static class Day1
+    {
+        public static async Task Solve()
+        {
+            using var sr = new StreamReader("Day1.txt");
+            var input = await sr.ReadToEndAsync();
+
+            var masses = input.Split(Environment.NewLine).Select(int.Parse);
+            var totalMass = masses.Sum(m => (int) Math.Floor((double) m / 3) - 2);
+
+            Console.WriteLine(totalMass);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Day1.Solve();
+
+            Console.ReadLine();
+        }
+    }
+}
