81be621927ae0b0155899fc6d3d8f9d87ce41d83
aevitas <m4rtijn@gmail.com>
2019-12-05 10:49:46 +0100

    Day two part one

---
+++ Day2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public static class Day2
+    {
+        public static async Task Solve()
+        {
+            var mem = (await File.ReadAllTextAsync("Day2.txt")).Split(",").Select(int.Parse).ToArray();
+
+            mem[1] = 12;
+            mem[2] = 2;
+
+            for (int i = 0; i < mem.Length;)
+            {
+                int opCode = mem[i];
+
+                if (opCode == 99)
+                    break;
+
+                if (opCode != 1 && opCode != 2)
+                    throw new NotSupportedException($"Unknown OpCode encountered: {opCode}");
+
+                var lhv = mem[mem[i + 1]]; // left operand
+                var rhv = mem[mem[i + 2]]; // right operand
+
+                mem[mem[i + 3]] = opCode == 1 ? lhv + rhv : lhv * rhv; // ret
+
+                i += 4;
+            }
+
+            Console.WriteLine(mem[0]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace AdventOfCode
     {
         static async Task Main(string[] args)
         {
-            await Day1.Solve();
+            await Day2.Solve();
 
             Console.ReadLine();
         }
