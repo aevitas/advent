ea2de65b656deb7cef5d0967645032d29a55fad8
aevitas <m4rtijn@gmail.com>
2019-12-09 18:08:51 +0100

    Day 4 part two

--- Day4.cs
+++ Day4.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace AdventOfCode
 {
@@ -8,14 +9,18 @@ namespace AdventOfCode
         {
             int min = 125730;
             int max = 579381;
-            int possibilities = 0;
+            int partOne = 0;
+            int partTwo = 0;
 
             for (int i = min; i < max; i++)
             {
                 var arr = i.ToString();
 
                 if (IsAscending(arr) && HasAdjacents(arr))
-                    possibilities += 1;
+                    partOne += 1;
+
+                if (IsAscending(arr) && HasPairAdjacents(arr))
+                    partTwo += 1;
             }
 
             bool IsAscending(string input)
@@ -40,7 +45,44 @@ namespace AdventOfCode
                 return false;
             }
 
-            Console.WriteLine(possibilities);
+            bool HasGroupAdjacents(string input)
+            {
+                for (int i = 0; i < input.Length - 2; i++)
+                {
+                    if (input[i] == input[i + 1] && input[i + 1] == input[i + 2])
+                        return true;
+                }
+
+                return false;
+            }
+
+            bool HasPairAdjacents(string input)
+            {
+                for (int i = 0; i < input.Length - 1; i++)
+                {
+                    if (input[i] != input[i + 1])
+                        continue;
+
+                    if (i + 2 < input.Length)
+                    {
+                        // Three or more of the same chars, skip them.
+                        if (input[i] == input[i + 1] && input[i + 1] == input[i + 2])
+                            i = i + 2;
+                    }
+
+                    // Last char
+                    if (i == input.Length - 1)
+                        continue;
+
+                    if (input[i] == input[i + 1])
+                        return true;
+                }
+
+                return false;
+            }
+
+            Console.WriteLine(partOne);
+            Console.WriteLine(partTwo);
         }
     }
 }
