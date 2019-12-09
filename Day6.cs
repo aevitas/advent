5b9f4e5dab51f04f82976cc8ebbc1e4c40f00837
aevitas <m4rtijn@gmail.com>
2019-12-09 15:56:22 +0100

    This coincidentally gives the correct answer for the demo set

--- Day6.cs
+++ Day6.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode
         {
             var input = File.ReadAllText("Day6.txt").Split(Environment.NewLine).Select(i => i.Split(')'));
 
-            var directOrbits = new Dictionary<string, HashSet<string>> {{"COM", new HashSet<string>()}};
+            var directOrbits = new Dictionary<string, HashSet<string>> { { "COM", new HashSet<string>() } };
 
             foreach (var i in input)
             {
@@ -29,11 +29,33 @@ namespace AdventOfCode
                 directOrbits[o].Add(i[1]);
             }
 
-            
-            foreach (var o in directOrbits)
+            // For each mass, get the planets they're orbiting. Keep doing that ad nauseam until we eventually run out. Count along the way.
+            var indirectOrbitCount = 0;
+            foreach (var mass in input.Distinct())
             {
+                var m = mass[1];
 
+                var count = 0;
+                bool any = true;
+                while (any)
+                {
+                    var linkedMasses = GetLinkedMasses(m);
+                    var c = linkedMasses.Sum(o => o.Value.Count);
+
+                    any = c > count;
+
+                    count += c;
+                }
+
+                indirectOrbitCount += count;
             }
+
+            List<KeyValuePair<string, HashSet<string>>> GetLinkedMasses(string mass)
+            {
+                return directOrbits.Where(o => o.Value.Contains(mass)).ToList();
+            }
+
+            Console.WriteLine(directOrbits.Count + indirectOrbitCount);
         }
     }
 }
