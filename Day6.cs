515b4e62b68a2343ee847f4dbb685fd15e732bb2
aevitas <m4rtijn@gmail.com>
2019-12-10 09:02:11 +0100

    Small stuff

--- Day6.cs
+++ Day6.cs
@@ -13,6 +13,16 @@ namespace AdventOfCode
 
             var directOrbits = new Dictionary<string, HashSet<string>> { { "COM", new HashSet<string>() } };
 
+            // Grab the orbits and arrange them outwards in - i.e. the key is the "orbiter", and the values are the masses we orbit.
+            // That way, when we calculate indirect orbits later on, we can simply iterate the values instead of having to do reverse lookups.
+            //
+            // Assuming C orbits B, orbits A, you'd get:
+            //
+            //           3     2    1
+            //          COM <- A <- B <- C
+            // 
+            // Where C's values can be enumerated to grab B, B's to grab A, etc.
+            //
             foreach (var i in input)
             {
                 var key = i[1];
@@ -30,11 +40,12 @@ namespace AdventOfCode
                 directOrbits[key].Add(value);
             }
 
-            // For each mass, get the planets they're orbiting. Keep doing that ad nauseam until we eventually run out. Count along the way.
-            foreach (var o in directOrbits)
+            // For each orbit, we grab its linked masses, and union their orbits with the current planet's orbit.
+            // Because A orbits B orbits C is three orbits, we'll end up with a fully inclusive list of all orbits, which we then simply have to count.
+            foreach (var orbit in directOrbits)
             {
-                foreach (var p in o.Value.ToList())
-                    o.Value.UnionWith(directOrbits[p]);
+                foreach (var linked in orbit.Value.ToArray())
+                    orbit.Value.UnionWith(directOrbits[linked]);
             }
 
             Console.WriteLine($"{directOrbits.Sum(o => o.Value.Count)}");
