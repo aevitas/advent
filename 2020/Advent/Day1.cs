d4b0842213440c659421ed2ead1fca7c3567a39f
aevitas <m4rtijn@gmail.com>
2020-12-02 14:55:17 +0100

    Day 1

---
+++ 2020/Advent/Day1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advent
+{
+    internal static class Day1
+    {
+        public static async Task SolveAsync()
+        {
+            using var sr = new StreamReader("Day1.txt");
+            var input = await sr.ReadToEndAsync();
+
+            var expenses = input.Split(Environment.NewLine).Select(int.Parse).ToArray();
+
+            for (int i = 0; i < expenses.Length; i++)
+            {
+                for (int y = 0;  y < expenses.Length; y++)
+                {
+                    if (y == i)
+                        continue;
+
+                    if (expenses[i] + expenses[y] == 2020)
+                    {
+                        Console.WriteLine(expenses[i] * expenses[y]);
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 2020/Advent/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Advent
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Day1.SolveAsync();
+
+            Console.ReadLine();
+        }
+    }
+}
