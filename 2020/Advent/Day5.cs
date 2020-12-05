4af50ffa30e3e0e9d3ff3c95fc1ee8b5e9585aa2
aevitas <m4rtijn@gmail.com>
2020-12-05 21:07:31 +0100

    Make the array creation slightly more succinct

--- 2020/Advent/Day5.cs
+++ 2020/Advent/Day5.cs
@@ -7,76 +7,69 @@ namespace Advent
 {
     internal static class Day5
     {
-        public static void Solve()
+    public static void Solve()
+    {
+        var sr = new StreamReader("Day5.txt");
+        var boardingPasses = sr.ReadToEnd().Split(Environment.NewLine);
+
+        var rows = Enumerable.Range(0, 128).ToArray();
+        var columns = Enumerable.Range(0, 8).ToArray();
+
+        var seatIds = new List<int>();
+        foreach (var p in boardingPasses)
         {
-            var sr = new StreamReader("Day5.txt");
-            var boardingPasses = sr.ReadToEnd().Split(Environment.NewLine);
-
-            var rows = new int[128];
-            for (int i = 0; i < 128; i++)
-                rows[i] = i;
-
-            var columns = new int[8];
-            for (int i = 0; i < 8; i++)
-                columns[i] = i;
-
-            var seatIds = new List<int>();
-            foreach (var p in boardingPasses)
+            var row = rows.ToArray();
+            var col = columns.ToArray();
+            foreach (var c in p)
             {
-                var row = rows.ToArray();
-                var col = columns.ToArray();
-                foreach (var c in p)
+                switch (c)
                 {
-                    switch (c)
-                    {
-                        case 'F':
-                            row = row.Take(row.Length / 2).ToArray();
-                            break;
-                        case 'B':
-                            row = row.Skip(row.Length / 2).Take(row.Length / 2).ToArray();
-                            break;
-                    }
-
-                    switch (c)
-                    {
-                        case 'L':
-                            col = col.Take(col.Length / 2).ToArray();
-                            break;
-                        case 'R':
-                            col = col.Skip(col.Length / 2).Take(col.Length / 2).ToArray();
-                            break;
-                    }
+                    case 'F':
+                        row = row.Take(row.Length / 2).ToArray();
+                        break;
+                    case 'B':
+                        row = row.Skip(row.Length / 2).Take(row.Length / 2).ToArray();
+                        break;
                 }
 
-                var id = row[0] * 8 + col[0];
-                seatIds.Add(id);
-            }
-
-            PartOne();
-            PartTwo();
-
-            void PartOne()
-            {
-                Console.WriteLine(seatIds.Max());
-            }
-
-            void PartTwo()
-            {
-                int? prev = null;
-                foreach (var s in seatIds.OrderBy(i => i))
+                switch (c)
                 {
-                    //Console.WriteLine(s);
-
-                    prev ??= s;
-
-                    if (s - prev > 1)
-                    {
-                        Console.WriteLine(s - 1);
-                    }
-
-                    prev = s;
+                    case 'L':
+                        col = col.Take(col.Length / 2).ToArray();
+                        break;
+                    case 'R':
+                        col = col.Skip(col.Length / 2).Take(col.Length / 2).ToArray();
+                        break;
                 }
+            }
+
+            var id = row[0] * 8 + col[0];
+            seatIds.Add(id);
+        }
+
+        PartOne();
+        PartTwo();
+
+        void PartOne()
+        {
+            Console.WriteLine(seatIds.Max());
+        }
+
+        void PartTwo()
+        {
+            int? prev = null;
+            foreach (var s in seatIds.OrderBy(i => i))
+            {
+                prev ??= s;
+
+                if (s - prev > 1)
+                {
+                    Console.WriteLine(s - 1);
+                }
+
+                prev = s;
             }
         }
+    }
     }
 }
