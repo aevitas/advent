5d208adb3ebc55fcee1a3b0c3a2db5fb38d86d29
aevitas <m4rtijn@gmail.com>
2020-12-05 21:07:47 +0100

    Fix indent

--- 2020/Advent/Day5.cs
+++ 2020/Advent/Day5.cs
@@ -7,69 +7,69 @@ namespace Advent
 {
     internal static class Day5
     {
-    public static void Solve()
-    {
-        var sr = new StreamReader("Day5.txt");
-        var boardingPasses = sr.ReadToEnd().Split(Environment.NewLine);
-
-        var rows = Enumerable.Range(0, 128).ToArray();
-        var columns = Enumerable.Range(0, 8).ToArray();
-
-        var seatIds = new List<int>();
-        foreach (var p in boardingPasses)
+        public static void Solve()
         {
-            var row = rows.ToArray();
-            var col = columns.ToArray();
-            foreach (var c in p)
+            var sr = new StreamReader("Day5.txt");
+            var boardingPasses = sr.ReadToEnd().Split(Environment.NewLine);
+
+            var rows = Enumerable.Range(0, 128).ToArray();
+            var columns = Enumerable.Range(0, 8).ToArray();
+
+            var seatIds = new List<int>();
+            foreach (var p in boardingPasses)
             {
-                switch (c)
+                var row = rows.ToArray();
+                var col = columns.ToArray();
+                foreach (var c in p)
                 {
-                    case 'F':
-                        row = row.Take(row.Length / 2).ToArray();
-                        break;
-                    case 'B':
-                        row = row.Skip(row.Length / 2).Take(row.Length / 2).ToArray();
-                        break;
+                    switch (c)
+                    {
+                        case 'F':
+                            row = row.Take(row.Length / 2).ToArray();
+                            break;
+                        case 'B':
+                            row = row.Skip(row.Length / 2).Take(row.Length / 2).ToArray();
+                            break;
+                    }
+
+                    switch (c)
+                    {
+                        case 'L':
+                            col = col.Take(col.Length / 2).ToArray();
+                            break;
+                        case 'R':
+                            col = col.Skip(col.Length / 2).Take(col.Length / 2).ToArray();
+                            break;
+                    }
                 }
 
-                switch (c)
-                {
-                    case 'L':
-                        col = col.Take(col.Length / 2).ToArray();
-                        break;
-                    case 'R':
-                        col = col.Skip(col.Length / 2).Take(col.Length / 2).ToArray();
-                        break;
-                }
+                var id = row[0] * 8 + col[0];
+                seatIds.Add(id);
             }
 
-            var id = row[0] * 8 + col[0];
-            seatIds.Add(id);
-        }
+            PartOne();
+            PartTwo();
 
-        PartOne();
-        PartTwo();
-
-        void PartOne()
-        {
-            Console.WriteLine(seatIds.Max());
-        }
-
-        void PartTwo()
-        {
-            int? prev = null;
-            foreach (var s in seatIds.OrderBy(i => i))
+            void PartOne()
             {
-                prev ??= s;
+                Console.WriteLine(seatIds.Max());
+            }
 
-                if (s - prev > 1)
+            void PartTwo()
+            {
+                int? prev = null;
+                foreach (var s in seatIds.OrderBy(i => i))
                 {
-                    Console.WriteLine(s - 1);
-                }
+                    prev ??= s;
 
-                prev = s;
+                    if (s - prev > 1)
+                    {
+                        Console.WriteLine(s - 1);
+                    }
+
+                    prev = s;
+                }
             }
         }
-    }
     }
 }
