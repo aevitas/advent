600b340182300f09c659b2d7c014cdacbf1099bc
aevitas <m4rtijn@gmail.com>
2020-12-08 10:37:51 +0100

    Finish this another time

--- 2020/Advent/Day7.cs
+++ 2020/Advent/Day7.cs
@@ -11,28 +11,35 @@ namespace Advent
     {
         public static async Task PartOneAsync()
         {
-            var bags = await GetBagsAsync();
+            var outer = await GetBagsAsync();
 
+            const string gold = "shiny gold";
+            var sorted = new HashSet<Bag>();
+            foreach (var b in outer)
+            {
+                foreach (var inner in b.Bags)
+                {
+                    sorted.Add(outer.First(o => o.Color == inner.Color));
+                }
 
+                sorted.Add(b);
+            }
         }
 
-        private static async Task<Dictionary<string, Bag[]>> GetBagsAsync()
+        private static async Task<HashSet<Bag>> GetBagsAsync()
         {
             using var sr = new StreamReader("Day7.txt");
             var input = (await sr.ReadToEndAsync()).Split(Environment.NewLine);
 
-            var bags = new Dictionary<string, Bag[]>();
+            var bags = new HashSet<Bag>();
 
             foreach (var s in input)
             {
-                var (color, innerBags) = ParseBags(s);
-
-                bags.Add(color, innerBags);
+                bags.Add(ParseBag(s));
             }
 
-            static (string color, Bag[] bags) ParseBags(string s)
+            static Bag ParseBag(string s)
             {
-                // Alright boys, get out of here.
                 s = s.TrimEnd('.');
                 s = s.Replace("bags", string.Empty);
                 s = s.Replace("bag", string.Empty);
@@ -40,14 +47,14 @@ namespace Advent
                 var outerBag = s.Split(" contain")[0].Trim();
 
                 if (s.Contains("no other"))
-                    return (outerBag, Array.Empty<Bag>());
+                    return new Bag {Color = outerBag, Bags = new List<Bag>()};
 
                 var remainingBags = s.Split("contain ")[1];
                 if (!s.Contains(','))
                 {
                     var inner = ParseBag(remainingBags);
 
-                    return (outerBag, new [] {inner});
+                    return new Bag{ Color = outerBag, Bags = new List<Bag>{inner}};
                 }
 
                 static Bag ParseBag(string line)
@@ -56,21 +63,21 @@ namespace Advent
                     var slots = int.Parse(s[..1]);
                     var color = s[2..].Trim();
 
-                    return new Bag {Color = color, Slots = slots};
+                    return new Bag {Color = color};
                 }
 
-                return (outerBag, remainingBags.Split(',').Select(ParseBag).ToArray());
+                return new Bag {Color = outerBag, Bags = remainingBags.Split(',').Select(ParseBag).ToList()};
             }
 
             return bags;
         }
 
-        [DebuggerDisplay("{Slots} {Color}")]
+        [DebuggerDisplay("{Color}")]
         private class Bag
         {
             public string Color { get; set; }
 
-            public int Slots { get; set; }
+            public List<Bag> Bags { get; set; }
         }
     }
 }
