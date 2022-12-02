ae7fbbcc590360fa686bdb54eb19170a4ed48a74
aevitas <aevitas@users.noreply.github.com>
2022-12-02 14:56:24 +0100

    Day 2 part one in go

--- 2022/Go/day2.go
+++ 2022/Go/day2.go
@@ -1,5 +1,58 @@
 package main
 
-func DayTwoPartOne() {
+import (
+	"fmt"
+	"strings"
+)
 
+func DayTwoPartOne() {
+	input := readInput("day2.txt")
+
+	om := make(map[string]string)
+	pm := make(map[string]string)
+
+	om["A"] = "Rock"
+	om["B"] = "Paper"
+	om["C"] = "Scissors"
+	pm["X"] = om["A"]
+	pm["Y"] = om["B"]
+	pm["Z"] = om["C"]
+
+	w := make(map[string]string)
+
+	w["Rock"] = "Scissors"
+	w["Paper"] = "Rock"
+	w["Scissors"] = "Paper"
+
+	ms := make(map[string]int)
+
+	ms["Rock"] = 1
+	ms["Paper"] = 2
+	ms["Scissors"] = 3
+
+	var score int
+	for _, line := range input {
+		split := strings.Split(line, " ")
+		o := om[split[0]]
+		p := pm[split[1]]
+
+		// We lose
+		if w[o] == p {
+			score += ms[p]
+			continue
+		}
+
+		// We win
+		if w[p] == o {
+			score += ms[p]
+			score += 6
+			continue
+		}
+
+		// Draw
+		score += ms[p]
+		score += 3
+	}
+
+	fmt.Println(score)
 }
