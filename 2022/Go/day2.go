0ffb302a4f76e3e135febc42c4a078c6a6cb15be
aevitas <aevitas@users.noreply.github.com>
2022-12-02 15:12:39 +0100

    Day 2 part 2 in go

--- 2022/Go/day2.go
+++ 2022/Go/day2.go
@@ -56,3 +56,69 @@ func DayTwoPartOne() {
 
 	fmt.Println(score)
 }
+
+func DayTwoPartTwo() {
+	input := readInput("day2.txt")
+
+	om := make(map[string]string)
+	pm := make(map[string]string)
+
+	om["A"] = "Rock"
+	om["B"] = "Paper"
+	om["C"] = "Scissors"
+	pm["X"] = om["A"]
+	pm["Y"] = om["B"]
+	pm["Z"] = om["C"]
+
+	w := make(map[string]string)
+
+	w["Rock"] = "Scissors"
+	w["Paper"] = "Rock"
+	w["Scissors"] = "Paper"
+
+	l := make(map[string]string)
+
+	l["Scissors"] = "Paper"
+	l["Rock"] = "Scissors"
+	l["Paper"] = "Rock"
+
+	ms := make(map[string]int)
+
+	ms["Rock"] = 1
+	ms["Paper"] = 2
+	ms["Scissors"] = 3
+
+	var score int
+	for _, line := range input {
+		split := strings.Split(line, " ")
+		o := om[split[0]]
+		p := split[1]
+
+		// We should lose
+		if p == "X" {
+			score += ms[l[o]]
+			continue
+		}
+
+		// We should draw
+		if p == "Y" {
+			score += ms[o]
+			score += 3
+			continue
+		}
+
+		var move string
+		for k, v := range w {
+			if v == o {
+				move = k
+				break
+			}
+		}
+
+		// We should win
+		score += ms[move]
+		score += 6
+	}
+
+	fmt.Println(score)
+}
