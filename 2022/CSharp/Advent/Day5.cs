710c568415a8463cee25e40d6e6bd7f1ee82c683
aevitas <aevitas@users.noreply.github.com>
2022-12-05 10:50:12 +0100

    Clean up a little

--- 2022/CSharp/Advent/Day5.cs
+++ 2022/CSharp/Advent/Day5.cs
@@ -1,51 +1,13 @@
-﻿using System.Collections;
-
-namespace Advent;
+﻿namespace Advent;
 
 public static class Day5
 {
     private static readonly string[] Input = File.ReadAllLines("Day5.txt");
-
-    /**
-            [D]    
-        [N] [C]    
-        [Z] [M] [P]
-        1   2   3 
-        
-     */
-
+    
     public static void PartOne()
     {
-        var stacks = new Dictionary<int, Stack<string>>();
-
-        for (int i = 1; i < 10; i++)
-            stacks[i] = new Stack<string>();
-
         var lines = Input.ToArray();
-        for (int l = lines.Length - 1; l >= 0; l--)
-        {
-            var line = lines[l];
-
-            if (line.Trim().StartsWith('1'))
-                continue;
-
-            if (line.Trim().StartsWith("move"))
-                continue;
-
-            int spaces = 0;
-            for (int i = 0; i < line.Length; i += 4)
-            {
-                var col = (i - spaces) / 3 + 1;
-                var box = line[i..(i + 3)].Replace("[", "").Replace("]", "").Trim();
-
-                spaces++;
-
-                if (string.IsNullOrWhiteSpace(box))
-                    continue;
-
-                stacks[col].Push(box);
-            }
-        }
+        var stacks = GetStacks();
 
         foreach (var l in lines)
         {
@@ -76,37 +38,9 @@ public static class Day5
 
     public static void PartTwo()
     {
-        var stacks = new Dictionary<int, Stack<string>>();
-
-        for (int i = 1; i < 10; i++)
-            stacks[i] = new Stack<string>();
-
         var lines = Input.ToArray();
-        for (int l = lines.Length - 1; l >= 0; l--)
-        {
-            var line = lines[l];
-
-            if (line.Trim().StartsWith('1'))
-                continue;
-
-            if (line.Trim().StartsWith("move"))
-                continue;
-
-            int spaces = 0;
-            for (int i = 0; i < line.Length; i += 4)
-            {
-                var col = (i - spaces) / 3 + 1;
-                var box = line[i..(i + 3)].Replace("[", "").Replace("]", "").Trim();
-
-                spaces++;
-
-                if (string.IsNullOrWhiteSpace(box))
-                    continue;
-
-                stacks[col].Push(box);
-            }
-        }
-
+        var stacks = GetStacks();
+        
         foreach (var l in lines)
         {
             if (!l.StartsWith("move"))
@@ -146,5 +80,41 @@ public static class Day5
         }
 
         Console.WriteLine();
+    }
+
+    private static Dictionary<int, Stack<string>> GetStacks()
+    {
+        var stacks = new Dictionary<int, Stack<string>>();
+
+        for (int i = 1; i < 10; i++)
+            stacks[i] = new Stack<string>();
+
+        var lines = Input.ToArray();
+        for (int l = lines.Length - 1; l >= 0; l--)
+        {
+            var line = lines[l];
+
+            if (line.Trim().StartsWith('1'))
+                continue;
+
+            if (line.Trim().StartsWith("move"))
+                continue;
+
+            int spaces = 0;
+            for (int i = 0; i < line.Length; i += 4)
+            {
+                var col = (i - spaces) / 3 + 1;
+                var box = line[i..(i + 3)].Replace("[", "").Replace("]", "").Trim();
+
+                spaces++;
+
+                if (string.IsNullOrWhiteSpace(box))
+                    continue;
+
+                stacks[col].Push(box);
+            }
+        }
+
+        return stacks;
     }
 }
