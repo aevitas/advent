96c13ef2f22e7fcd5b1e1ef90fecf49c4afe239e
aevitas <aevitas@users.noreply.github.com>
2022-12-08 13:05:03 +0100

    Day 8 part two

--- 2022/CSharp/Advent/Day8.cs
+++ 2022/CSharp/Advent/Day8.cs
@@ -10,52 +10,111 @@ public static class Day8
     {
         var grid = CreateHeightmap(out var bounds);
         var visible = new Dictionary<(int, int), bool>();
-        
+
         for (int y = 0; y < bounds.Y; y++)
         {
             for (int x = 0; x < bounds.X; x++)
             {
                 var height = grid[(x, y)];
                 var v = true;
-                for (int j = 0; v && j < bounds.X; j++)
+                for (int i = 0; v && i < bounds.X; i++)
                 {
-                    var cur = grid[(j, y)];
+                    var cur = grid[(i, y)];
                     if (cur >= height)
-                        visible[(j, y)] = v = false;
-                }
-                
-                v = true;
-                for (int j = 0; v && j < bounds.Y; j++)
-                {
-                    var cur = grid[(x, j)];
-                    if (cur >= height)
-                        visible[(x, j)] = v = false;
+                        visible[(i, y)] = v = false;
                 }
 
                 v = true;
-                for (int j = (int) bounds.X - 1; v && j > 0; j--)
+                for (int i = 0; v && i < bounds.Y; i++)
                 {
-                    var cur = grid[(j, y)];
+                    var cur = grid[(x, i)];
                     if (cur >= height)
-                        visible[(j, y)] = v = false;
+                        visible[(x, i)] = v = false;
                 }
 
                 v = true;
-                for (int j = (int)bounds.Y - 1; v && j > 0; j--)
+                for (int i = (int)bounds.X - 1; v && i > 0; i--)
                 {
-                    var cur = grid[(x, j)];
+                    var cur = grid[(i, y)];
                     if (cur >= height)
-                        visible[(x, j)] = v = false;
+                        visible[(i, y)] = v = false;
+                }
+
+                v = true;
+                for (int i = (int)bounds.Y - 1; v && i > 0; i--)
+                {
+                    var cur = grid[(x, i)];
+                    if (cur >= height)
+                        visible[(x, i)] = v = false;
                 }
             }
         }
-        
+
         Console.WriteLine(visible.Count);
     }
 
     public static void PartTwo()
     {
+        var grid = CreateHeightmap(out var bounds);
+        int max = 0;
 
+        for (int y = 0; y < bounds.Y; y++)
+        {
+            for (int x = 0; x < bounds.X; x++)
+            {
+                var height = grid[(x, y)];
+                var score = 1;
+
+                var c = 0;
+                for (int i = x + 1; i < bounds.X; i++)
+                {
+                    c++;
+                    var cur = grid[(i, y)];
+                    if (cur >= height)
+                        break;
+                }
+
+                score *= c;
+                c = 0;
+
+                for (int i = y + 1; i < bounds.Y; i++)
+                {
+                    c++;
+                    var cur = grid[(x, i)];
+                    if (cur >= height)
+                        break;
+                }
+
+                score *= c;
+                c = 0;
+
+                for (int i = x - 1; i >= 0; i--)
+                {
+                    c++;
+                    var cur = grid[(i, y)];
+                    if (cur >= height)
+                        break;
+                }
+
+                score *= c;
+                c = 0;
+
+                for (int i = y - 1; i >= 0; i--)
+                {
+                    c++;
+                    var cur = grid[(x, i)];
+                    if (cur >= height)
+                        break;
+                }
+
+                score *= c;
+
+                if (score > max)
+                    max = score;
+            }
+        }
+
+        Console.WriteLine(max);
     }
 
     private static Dictionary<(int, int), int> CreateHeightmap(out Vector2 bounds)
