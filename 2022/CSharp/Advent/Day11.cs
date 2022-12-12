9662f4673eb06ec14ba5d5f45e4b5a7b9f185ba5
aevitas <aevitas@users.noreply.github.com>
2022-12-12 11:49:13 +0100

    Day 11 part two

--- 2022/CSharp/Advent/Day11.cs
+++ 2022/CSharp/Advent/Day11.cs
@@ -1,11 +1,11 @@
-﻿namespace Advent;
+﻿using System.Numerics;
+
+namespace Advent;
 
 public static class Day11
 {
     public static string[] Input = File.ReadAllLines("Day11.txt").Where(l => !string.IsNullOrWhiteSpace(l)).ToArray();
-
-    private static readonly Action<string> Log = s => { };
-
+    
     public static void PartOne()
     {
         var monkeys = GetMonkeys();
@@ -15,27 +15,19 @@ public static class Day11
         {
             foreach (var monkey in monkeys)
             {
-                Log($"Monkey {monkey.Key}");
                 var m = monkey.Value;
                 while (m.Items.TryDequeue(out var j))
                 {
-                    Log($"\tMonkey inspects an item with a worry level of {j}.");
                     var worry = m.Operation(j);
-                    Log($"\t\tWorry level increased to {worry}");
                     var bored = worry / 3;
-                    Log($"\t\tMonkey gets bored with item. Worry level is divided by 3 to {bored}.");
 
                     if (bored % m.Divisor == 0)
                     {
-                        Log($"\t\tCurrent worry level is divisible by {m.Divisor}.");
                         monkeys[m.SuccessMonkey].Items.Enqueue(bored);
-                        Log($"\t\tItem with worry level {bored} is thrown to monkey {m.SuccessMonkey}.");
                     }
                     else
                     {
-                        Log($"\t\tCurrent worry level is not divisible by {m.Divisor}.");
                         monkeys[m.FailMonkey].Items.Enqueue(bored);
-                        Log($"\t\tItem with worry level {bored} is thrown to monkey {m.FailMonkey}.");
                     }
 
                     if (!inspections.ContainsKey(monkey.Key))
@@ -45,16 +37,7 @@ public static class Day11
                 }
             }
         }
-
-        foreach (var m in monkeys)
-        {
-            Log($"Monkey {m.Key}: ");
-            foreach (var i in m.Value.Items)
-                Log($"{i} ");
-
-            Log("");
-        }
-
+        
         var r = inspections.OrderByDescending(i => i.Value).Take(2).ToArray();
 
         Console.WriteLine(r[0].Value * r[1].Value);
@@ -62,6 +45,38 @@ public static class Day11
 
     public static void PartTwo()
     {
+        var monkeys = GetMonkeys();
+        var inspections = new Dictionary<int, int>();
+
+        for (int i = 0; i < 10000; i++)
+        {
+            foreach (var monkey in monkeys)
+            {
+                var m = monkey.Value;
+                while (m.Items.TryDequeue(out var j))
+                {
+                    var worry = m.Operation(j);
+
+                    if (worry % m.Divisor == 0)
+                    {
+                        monkeys[m.SuccessMonkey].Items.Enqueue(worry);
+                    }
+                    else
+                    {
+                        monkeys[m.FailMonkey].Items.Enqueue(worry);
+                    }
+
+                    if (!inspections.ContainsKey(monkey.Key))
+                        inspections[monkey.Key] = 0;
+
+                    inspections[monkey.Key]++;
+                }
+            }
+        }
+        
+        var r = inspections.OrderByDescending(i => i.Value).Take(2).ToArray();
+        
+        Console.WriteLine(new BigInteger(r[0].Value) * new BigInteger(r[1].Value));
 
     }
 
@@ -72,9 +87,9 @@ public static class Day11
         for (int i = 0; i < Input.Length; i += 6)
         {
             var m = Input[i].Trim().Split(' ')[1].Trim(':');
-            var items = Input[i + 1].Trim().Split(':')[1].Split(',').Select(o => int.Parse(o.Trim())).ToArray();
+            var items = Input[i + 1].Trim().Split(':')[1].Split(',').Select(o => long.Parse(o.Trim())).ToArray();
             var operand = Input[i + 2].Trim().Split(' ')[5];
-            Func<int, int> operation = _ => throw new ArgumentException();
+            Func<long, long> operation = _ => throw new ArgumentException();
             var op = Input[i + 2].Trim();
 
             if (op.Contains('*'))
@@ -123,7 +138,7 @@ public static class Day11
                 Divisor = int.Parse(divisor),
                 FailMonkey = int.Parse(f),
                 SuccessMonkey = int.Parse(t),
-                Items = new Queue<int>(items),
+                Items = new Queue<long>(items),
                 Number = monkey,
                 Operation = operation
             };
@@ -137,9 +152,9 @@ public class Monkey
 {
     public required int Number { get; init; }
 
-    public required Queue<int> Items { get; init; }
+    public required Queue<long> Items { get; init; }
 
-    public required Func<int, int> Operation { get; init; }
+    public required Func<long, long> Operation { get; init; }
 
     public required int Divisor { get; init; }
 
